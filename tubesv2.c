2f4e85769362ae1c150caa8629b54d91c659e829
cekingx <cekingx@gmail.com>
2017-11-23 13:49:49 +0800

    loop success

--- tubesv2.c
+++ tubesv2.c
@@ -2,51 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-FILE *file;
-typedef struct{
-       int tanggal;
-       int bulan;
-       int tahun;
-}Date;
-
 typedef struct{
        char nama[30];
-       char no_ktp[30];
+       char ktp[30];
        char alamat[50];
-       Date tanggal_sewa;
-       Date tanggal_kembali;
-}Penyewa;
+}Penyewaan;
 
-Penyewa penyewa[100];
+int indeks_sewa=0;
+FILE *file;
+Penyewaan arrsewa[50];
 
-void sewa();
-char con_tanggal();
+void input_data(int i);
+void sewa_save_data();
 
 int main(){
-       char str1[10],str2[10];
-       int i=0,j,str;
-       do{
-              strcpy(str1,"0");
-              printf("Masukkan nama : "); gets_s(penyewa[i].nama,20);
-              printf("Masukkan no_ktp : "); gets_s(penyewa[i].no_ktp,20);
-              printf("Masukkan alamat : "); gets_s(penyewa[i].alamat,20);
-              sewa(i);
-              i++;
-              j=strcmp(penyewa[i].nama,str1);
-              itoa(j,str2,10);
-              str=strcmp(str1,str2);
-       }while(str!=0);
+       sewa_save_data();
+       printf("Thank You\n");
 
        return 0;
 }
 
-void sewa(int i){
-       file=fopen("Database/sewa.txt","a+");
-       if (file == NULL){
-              printf("Error");
-       }
-       else{
-              fprintf(file, "%s\t%s\t%s\t",penyewa[i].nama, penyewa[i].no_ktp, penyewa[i].alamat);
-       }
-       fclose(file);
+void input_data(int i){
+       printf("Nama > ");scanf("%s",arrsewa[i].nama);
+       printf("KTP > ");scanf("%s",arrsewa[i].ktp);
+       printf("Alamat > ");scanf("%s",arrsewa[i].alamat);
+}
+
+void sewa_save_data(){
+       char pilih;
+       do{
+              printf("Input data ?[y/t] > ");
+              scanf("%c",&pilih);
+              if(pilih == 'y' || pilih == 'Y'){
+                     input_data(indeks_sewa);
+                     indeks_sewa++;
+              }
+              else{
+                     getchar();
+                     break;
+              }
+       }while(pilih == 'y' || pilih == 'Y');
 }
